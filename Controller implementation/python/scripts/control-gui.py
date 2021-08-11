36f5fe9ce9ed694dd66efa58e4f30f5bd6a9a766
Elkinmt19 <elkin02011@hotmail.com>
2021-08-10 20:18:11 -0500

    Added the requirements.txt file to the repositoy and also created a new struture for the Controller implementation directory

---
+++ Controller implementation/python/scripts/control-gui.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# Built-in imports 
+import time 
+import tkinter as tk
+import collections
+from threading import Thread 
+
+# External imports 
+import serial 
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+import matplotlib.animation as animation 
+
+
+class ControlGui:
+    def __ini__(self):
+        pass
